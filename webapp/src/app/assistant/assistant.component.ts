bab25283d6a7bfd040a0d1c03a877674f5ba447f
jpozarycki <jakub.pozarycki@gmail.com>
2020-03-21 12:07:15 +0100

    initial assistant work

--- webapp/src/app/assistant/assistant.component.ts
+++ webapp/src/app/assistant/assistant.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataStorageService } from '../data-storage/data-storage.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-assistant',
   templateUrl: './assistant.component.html',
   styleUrls: ['./assistant.component.scss']
 })
-export class AssistantComponent implements OnInit {
+export class AssistantComponent implements OnInit, OnDestroy {
+  overallNumberOfWords: number | undefined;
+  numberOfFlagWords: number | undefined;
+  flagWordsPercentage: number | undefined;
+  overallNumberOfWords$: Subscription;
+  numberOfFlagWords$: Subscription;
 
-  constructor() { }
+  constructor(private readonly dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
+    this.numberOfFlagWords$ = this.dataStorageService.numberOfInformalWordsAndPhrases.subscribe(data => {
+      this.numberOfFlagWords = data;
+      this.recalculatePercentageOfInformalWords();
+    });
+    this.overallNumberOfWords$ = this.dataStorageService.overallNumberOfWords.subscribe(data => {
+      this.overallNumberOfWords = data;
+      this.recalculatePercentageOfInformalWords();
+    });
   }
 
+  ngOnDestroy(): void {
+    this.numberOfFlagWords$.unsubscribe();
+    this.overallNumberOfWords$.unsubscribe();
+  }
+
+  private recalculatePercentageOfInformalWords(): void {
+    if (this.numberOfFlagWords !== undefined && this.overallNumberOfWords !== undefined) {
+      this.flagWordsPercentage = this.numberOfFlagWords / this.overallNumberOfWords * 100;
+    }
+    console.log(this.flagWordsPercentage);
+  }
 }
