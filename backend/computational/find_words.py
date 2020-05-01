a25a75c3113b50356ec38e61eb053ec10e62e0da
jpozarycki <jakub.pozarycki@gmail.com>
2020-05-01 14:21:50 +0200

    updated api point to send formality score

--- backend/computational/find_words.py
+++ backend/computational/find_words.py
@@ -2,76 +2,6 @@ import time
 import os
 import json
 
-informal_words = []
-allowed_chars = [' ', '-', ',', '.', ';', ':', '?', '!', '/', '+', '=', '(', ')', '{', '}', '[', ']', '"', '*', '^', '$', '#', '@', '`', '\n']
-
-def find_all_indices(input_str, search_str):
-    word_indices = []
-    length = len(input_str)
-    index = 0
-    while index < length:
-        i = input_str.find(search_str, index)
-        if i == -1:
-            return word_indices
-        elif i > 0:
-            preceeding_char = input_str[i-1]
-            following_char = input_str[i + len(search_str)]
-            if preceeding_char in allowed_chars and following_char in allowed_chars:
-                word_indices.append(i)
-        else: # case of first word in text
-            following_char = input_str[i + len(search_str)]
-            if following_char in allowed_chars:
-                word_indices.append(i)
-        index = i + 1
-    return word_indices
-
-def find_informal_words(text):
-    text = text.lower()
-    if informal_words == []:
-        load_informal_words_set()
-    indices = []
-    for word in informal_words:
-        word_indices = []
-        word_indices = find_all_indices(text, word)
-        if word_indices != []:
-            print("word: ", word, " -> ", word_indices)
-            for word_index in word_indices:
-                for i in range(len(word)):
-                    indices.append(word_index + i)
-
-    indices = list( dict.fromkeys(indices) )
-    indices.sort()
-    print("Indices: ", indices)
-    results = {"indices": indices}
-    return results
-
-def load_informal_words_set():
-    informal_words_filename = 'informal_words.csv'
-    informal_words_path = os.path.join(os.path.dirname(__file__), informal_words_filename)
-    file = open(informal_words_path, 'r+')
-    contents = file.readlines()
-
-    for line in contents:
-        line = line.replace("\n", "").lower()
-        informal_words.append(line)
-    file.close()
-
-# Testing
-# test_file = open("test_text.txt", "r")
-# test_text = test_file.read()
-# find_informal_words(test_text)
-# test_file.close()
-
-################################################
-# WARNING: Above methods should not be used anymore.
-# Below are given the proper ones, that at the same time compute the formality score.
-################################################
-
-"""
-import time
-import os
-import json
-
 class TextAnalyzer:
 
     def __init__(self, text):
@@ -155,7 +85,7 @@ class TextAnalyzer:
             clean_text = clean_text.replace(char, ' ')
         splitted_text = clean_text.split()
         self.formality_score = (self.informal_words_counter / len(splitted_text)) * 100
-
+"""
 # Example usage
 test_file = open("test_text.txt", "r")
 test_text = test_file.read()
